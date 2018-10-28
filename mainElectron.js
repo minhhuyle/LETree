dbf443a7bee436a9118b63caf411e0143d50380b
Minh-Huy LE <minhhuylecontact@gmail.com>
2018-10-28 14:58:12 +0100

    add angular to project

--- mainElectron.js
+++ mainElectron.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow } = require('electron');
+const path = require("path");
+const url = require("url");
 
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the index.html of the app.
-  win.loadFile('src/index.html');
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
 
   // Open the DevTools.
   //win.webContents.openDevTools();
